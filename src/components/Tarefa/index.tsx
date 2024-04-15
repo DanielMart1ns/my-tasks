b47bbe21417a4879e4f864d9a8c50a64f71724a8
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-04-15 16:37:38 -0300

    feat: task area created

---
+++ src/components/Tarefa/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './style'
+const Tarefa = () => (
+  <S.Card>
+    <S.Titulo>Nome da tarefa</S.Titulo>
+
+    <S.Tag>importante</S.Tag>
+    <S.Tag>pendente</S.Tag>
+
+    <S.Descricao />
+
+    <S.Acoes>
+      <S.Botao>Editar</S.Botao>
+      <S.Botao>Remover</S.Botao>
+    </S.Acoes>
+  </S.Card>
+)
+
+export default Tarefa
