fcf2123899fe05d25e9a4bd781b6b416c768ae46
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-04-16 16:14:21 -0300

    feat: componentized tags

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import * as S from './style'
+import * as enums from '../../utils/enums/Tarefa'
 
 type Props = {
   titulo: string
-  prioridade: string
-  status: string
+  prioridade: enums.Prioridade
+  status: enums.Status
   descricao: string
 }
 
@@ -15,21 +16,27 @@ const Tarefa = ({ titulo, prioridade, status, descricao }: Props) => {
     <S.Card>
       <S.Titulo>{titulo}</S.Titulo>
 
-      <S.Tag>{prioridade}</S.Tag>
-      <S.Tag>{status}</S.Tag>
+      <S.Tag parametro="prioridade" prioridade={prioridade}>
+        {prioridade}
+      </S.Tag>
+      <S.Tag parametro="status" status={status}>
+        {status}
+      </S.Tag>
 
       <S.Descricao value={descricao} />
 
       <S.Acoes>
         {estaEditando ? (
           <>
-            <S.Botao>Salvar</S.Botao>
-            <S.Botao onClick={() => setEstaEditando(false)}>Cancelar</S.Botao>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCancelarRemover onClick={() => setEstaEditando(false)}>
+              Cancelar
+            </S.BotaoCancelarRemover>
           </>
         ) : (
           <>
             <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
-            <S.Botao>Remover</S.Botao>
+            <S.BotaoCancelarRemover>Remover</S.BotaoCancelarRemover>
           </>
         )}
       </S.Acoes>
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,24 +1,25 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
 
 const tarefas = [
   {
     titulo: 'Estudar ts',
     descricao: 'Ver aula',
-    prioridade: 'Importante',
-    status: 'Pendente'
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
   },
   {
     titulo: 'Pagar conta de internet',
     descricao: 'Baixar fatura no Gmail',
-    prioridade: 'Urgente',
-    status: 'Concluída'
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
   },
   {
     titulo: 'Ir para a academia',
     descricao: 'Fazer treino x',
-    prioridade: 'importante',
-    status: 'Concluída'
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.CONCLUIDA
   }
 ]
 
