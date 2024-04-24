a43faeb3809d5d29fc029412cb8b7215c70aacbb
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-04-24 14:15:50 -0300

    feat: using cards to filter

--- src/components/FiltroCard/index.tsx
+++ src/components/FiltroCard/index.tsx
@@ -1,16 +1,56 @@
+import { useDispatch, useSelector } from 'react-redux'
 import * as S from './styles'
+import { alterarFiltro } from '../../store/reducers/filtro'
+import * as enums from '../../utils/enums/Tarefa'
+import { RootReducer } from '../../store'
 
 export type Props = {
-  ativo?: boolean
-  contador: number
   legenda: string
+  criterio: 'prioridade' | 'status' | 'todas'
+  valor?: enums.Prioridade | enums.Status
 }
 
-const FiltroCard = ({ ativo, contador, legenda }: Props) => (
-  <S.Card ativo={ativo}>
-    <S.Contador>{contador}</S.Contador>
-    <S.Label>{legenda}</S.Label>
-  </S.Card>
-)
+const FiltroCard = ({ legenda, criterio, valor }: Props) => {
+  const dispatch = useDispatch()
+  const { filtro, tarefas } = useSelector((state: RootReducer) => state)
+
+  const verificaEstaAtivo = () => {
+    const mesmoCriterio = filtro.criterio === criterio
+    const mesmoValor = filtro.valor === valor
+
+    return mesmoCriterio && mesmoValor //vai retornar um boolean
+  }
+
+  const ativo = verificaEstaAtivo()
+
+  const filtrar = () => {
+    dispatch(
+      alterarFiltro({
+        criterio: criterio,
+        valor //como o valor é igual ao nome eu posso inserir só o nome
+      })
+    )
+  }
+
+  const contarTarefas = () => {
+    //vai retornar o comprimento ou seja a quantidade
+    if (criterio === 'todas') return tarefas.itens.length
+    if (criterio === 'prioridade') {
+      return tarefas.itens.filter((item) => item.prioridade === valor).length
+    }
+    if (criterio === 'status') {
+      return tarefas.itens.filter((item) => item.status === valor).length
+    }
+  }
+
+  const contador = contarTarefas()
+
+  return (
+    <S.Card ativo={ativo} onClick={filtrar}>
+      <S.Contador>{contador}</S.Contador>
+      <S.Label>{legenda}</S.Label>
+    </S.Card>
+  )
+}
 
 export default FiltroCard
